488887d3d9ffd23045caa3c25cfe7fea279fd4ef
n4lkin <kalugin.serej1@gmail.com>
2022-08-05 13:06:04 +0300

    feat: now the arguments are taken from Settings

--- database/connection.py
+++ database/connection.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://canalservice:super_password1@0.0.0.0/db"
+from config import Settings
+
+SQLALCHEMY_DATABASE_URL = Settings.SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=Settings.DB_AUTOCOMMIT, autoflush=Settings.DB_AUTOFLUSH, bind=engine)
 
 Base = declarative_base()
