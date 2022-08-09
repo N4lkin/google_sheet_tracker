e7a513b5889aa181c4bd69d9794b5255e2112a43
n4lkin <kalugin.serej1@gmail.com>
2022-08-09 12:51:12 +0300

    feat: now GOOGLE_APPLICATION_CREDENTIALS start automatically

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import os
+
 import decouple
 
 
 class Settings:
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     CREDENTIALS_FILE = decouple.config("CREDENTIALS_FILE")
     SPREADSHEET_ID = decouple.config("SPREADSHEET_ID")
     POSTGRES_NAME = decouple.config("POSTGRES_NAME")
@@ -9,7 +12,7 @@ class Settings:
     POSTGRES_HOST = decouple.config("POSTGRES_HOST")
     POSTGRES_PORT = decouple.config("POSTGRES_PORT")
     POSTGRES_DB = decouple.config("POSTGRES_DB")
-    GOOGLE_APPLICATION_CREDENTIALS = decouple.config("GOOGLE_APPLICATION_CREDENTIALS")
+    GOOGLE_APPLICATION_CREDENTIALS = ROOT_DIR + "/credentails.json"
     CBRF_DAILY = decouple.config("CBRF_DAILY")
     TG_BOT_KEY = decouple.config("TG_BOT_KEY")
     REDIS_PORT = decouple.config("REDIS_PORT")
